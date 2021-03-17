f124efd9ae81cd79c22304adc3dd20f515f104d6
thaiKari <karimelingjohannessen@gmail.com>
2021-03-17 22:46:02 +0100

    call the api

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
+  
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [apiMessage, setApiMessage] = useState()
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const apires = await axios(
+        '/api/function1'
+      );
+
+      setApiMessage(apires.data);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      Hi
+      Hi. Here's something from the api:
+      {apiMessage}
     </div>
   );
 }
